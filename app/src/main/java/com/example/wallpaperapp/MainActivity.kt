a5116b811730b93a2bbcdea9dbc925730c72d313
Jasur Alimov <95330730+AndroidDeveloper257@users.noreply.github.com>
2024-02-09 19:15:16 +0500

    blur for bottom bar is in progress

--- app/src/main/java/com/example/wallpaperapp/MainActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.wallpaperapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.wallpaperapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            setContentView(root)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                appBarMain.bottomCard.setRenderEffect(
+                    RenderEffect.createBlurEffect(
+                        10F,
+                        10F,
+                        Shader.TileMode.MIRROR
+                    )
+                )
+            }
+        }
     }
 }
